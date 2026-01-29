8b53dc9ec5a7d78cf1c6313502d0739f0471659c
Christianst <sammy.rohde@gmx.de>
2026-01-29 17:03:18 +0100

    Removing the N8B Server from the code.

---
+++ cogs/removen8b.py
@@ -0,0 +1,16 @@
+from discord.ext import commands
+
+class RemoveN8BCommand(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name="removen8b")
+    async def removen8b(self, ctx):
+        user = ctx.author
+
+        target_guild = self.bot.get_guild(1363137083148865598)
+        if target_guild is None:
+            ctx.channel.send("Server nicht gefunden.", delete_after=10)
+        else:
+            target_guild.leave()
+            ctx.channel.send("Hell Cat hat N8B verlassen.", delete_after=10)
